c76d858c80e693ab571ed8a5ce4f2ff3a678328a
icnpdesign <inp@bu.edu>
2017-11-12 20:33:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array) {
+  return array.map(function(e){
+    return e.toLowerCase()
+  })
+}
+
+function nameToAttributes(names) {
+  return names.map(function(name) {
+    let fName = name.split (" ")[0];
+    let lName = name.split (" ")[1];
+    return {firstName: fName, lastName: lName}
+  })
+}
+
+function attributesToPhrase(names) {
+    return names.map(function(name) {
+    return `${name} is from ${hometown}`;
+  } )
+}
